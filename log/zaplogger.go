10e6cc6356da2438417e7bae12f64c54705bf69d
bennysong <bennysong@tencent.com>
2022-08-01 01:26:18 +0800

    init

---
+++ log/zaplogger.go
@@ -0,0 +1,59 @@
+package log
+
+import (
+	"go.uber.org/zap"
+)
+
+var sugar *zap.SugaredLogger
+
+// Debug logs to DEBUG log. Arguments are handled in the manner of fmt.Print.
+func Debug(args ...interface{}) {
+	sugar.Debug(args...)
+}
+
+// Debugf logs to DEBUG log. Arguments are handled in the manner of fmt.Printf.
+func Debugf(format string, args ...interface{}) {
+	sugar.Debugf(format, args...)
+}
+
+// Info logs to INFO log. Arguments are handled in the manner of fmt.Print.
+func Info(args ...interface{}) {
+	sugar.Info(args...)
+}
+
+// Infof logs to INFO log. Arguments are handled in the manner of fmt.Printf.
+func Infof(format string, args ...interface{}) {
+	sugar.Infof(format, args...)
+}
+
+// Warn logs to WARNING log. Arguments are handled in the manner of fmt.Print.
+func Warn(args ...interface{}) {
+	sugar.Warn(args...)
+}
+
+// Warnf logs to WARNING log. Arguments are handled in the manner of fmt.Printf.
+func Warnf(format string, args ...interface{}) {
+	sugar.Warnf(format, args...)
+}
+
+// Error logs to ERROR log. Arguments are handled in the manner of fmt.Print.
+func Error(args ...interface{}) {
+	sugar.Error(args...)
+}
+
+// Errorf logs to ERROR log. Arguments are handled in the manner of fmt.Printf.
+func Errorf(format string, args ...interface{}) {
+	sugar.Errorf(format, args...)
+}
+
+// Fatal logs to ERROR log. Arguments are handled in the manner of fmt.Print.
+// that all Fatal logs will exit with os.Exit(1).
+// Implementations may also call os.Exit() with a non-zero exit code.
+func Fatal(args ...interface{}) {
+	sugar.Fatal(args...)
+}
+
+// Fatalf logs to ERROR log. Arguments are handled in the manner of fmt.Printf.
+func Fatalf(format string, args ...interface{}) {
+	sugar.Fatalf(format, args...)
+}
